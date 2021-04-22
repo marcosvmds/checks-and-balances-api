a013b41d8711e6a9c3a8fdf47ccaff3734dbeac3
MarcosMazzei <mazzeimarcosv@gmail.com>
2021-04-22 02:21:51 -0300

    bugfix

--- config/db.js
+++ config/db.js
@@ -2,7 +2,5 @@ const enviroment = process.env.DB_ENV || 'development'
 const config = require('../knexfile.js')[enviroment]
 const knex = require('knex')(config)
 
-console.log("chega aqui? ")
-console.log(process.env.DATABASE_URL)
 knex.migrate.latest([config])
 module.exports = knex
--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,18 @@
 module.exports = {
   production: {
     client: 'pg',
-    connection: {
+    connection: () => ({
       connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false }
-    },
+    }),
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
       tableName: 'knex_migrations'
-    }
+    },
+    debug: true 
   },
   development: {
     client: 'postgresql',
