9116208a61282a7201f6766e99042cdac2c8e5bb
MarcosMazzei <mazzeimarcosv@gmail.com>
2021-04-22 03:30:19 -0300

    bugfix: testing knex file client change

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
 module.exports = {
   production: {
-    client: 'pg',
-    connection: () => ({
+    client: 'postgresql',
+    connection: {
       connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false }
-    }),
+    },
     pool: {
       min: 2,
       max: 10
